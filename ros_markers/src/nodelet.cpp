e1fe069de8b3bb9e7d5927c048408ac0c502cc21
Alberto Romay <romay@sim.tu-darmstadt.de>
2017-02-27 13:15:42 +0100

    Added interface for activating detection using ros action

--- ros_markers/src/nodelet.cpp
+++ ros_markers/src/nodelet.cpp
@@ -5,6 +5,12 @@
 
 #include "chilitagsdetector.hpp"
 
+#include <actionlib/server/simple_action_server.h>
+#include <ros_markers_msgs/RosMarkersAction.h>
+
+
+typedef actionlib::SimpleActionServer<ros_markers_msgs::RosMarkersAction> RosMarkersActionServer;
+
 
 using namespace std;
 
@@ -25,23 +31,33 @@ public:
     NODELET_INFO("Stopping chilitags detection");
   }
 
+  void rosMarkersDetectionGoalCallback();
+  void rosMarkersDetectionPreemptCallback();
+
 private:
   virtual void onInit();
   boost::shared_ptr<ChilitagsDetector> chilitags_;
+  boost::shared_ptr<RosMarkersActionServer> ros_markers_as_ptr_;
+  RosMarkersActionServer::GoalConstPtr ros_markers_goal_ptr_;
+
+
+  ros::NodeHandle rosNode;
+  string configFilename;
+  string tfParentFrame;
+  double defaultTagSize;
+  bool omitOtherTags;
 };
 
 void ChilitagsNodelet::onInit()
 {
     //ROS initialization
-    ros::NodeHandle rosNode(getNodeHandle());
+    rosNode = getNodeHandle();
     ros::NodeHandle _private_node(getPrivateNodeHandle());
 
     // load parameters
-    string configFilename;
     _private_node.param<string>("markers_configuration", configFilename, "");
-    double defaultTagSize;
+    _private_node.param<string>("tf_parent_frame", tfParentFrame, "");
     _private_node.param<double>("default_marker_size", defaultTagSize, USE_CHILITAGS_DEFAULT_PARAM);
-    bool omitOtherTags;
     _private_node.param<bool>("omit_other_tags", omitOtherTags, false);
 
     if (configFilename.empty() && omitOtherTags == true) {
@@ -49,10 +65,25 @@ void ChilitagsNodelet::onInit()
                              "omitOtherTags must not be set to true or no tags will be detected.");
     }
 
-    // initialize the detector by subscribing to the camera video stream
-    chilitags_.reset(new ChilitagsDetector(rosNode, configFilename, omitOtherTags, defaultTagSize));
-    ROS_INFO("ros_markers nodelet is ready. Marker locations will be published on TF when detected.");
+    ros_markers_as_ptr_.reset(new RosMarkersActionServer(_private_node, "ros_markers_detection", 0, false));
 
+    ros_markers_as_ptr_->registerGoalCallback(boost::bind(&ChilitagsNodelet::rosMarkersDetectionGoalCallback, this));
+    ros_markers_as_ptr_->registerPreemptCallback(boost::bind(&ChilitagsNodelet::rosMarkersDetectionPreemptCallback, this));
+
+    ros_markers_as_ptr_->start();
+}
+
+void ChilitagsNodelet::rosMarkersDetectionGoalCallback(){
+    ros_markers_goal_ptr_ = ros_markers_as_ptr_->acceptNewGoal();
+    // initialize the detector by subscribing to the camera video stream
+    chilitags_.reset(new ChilitagsDetector(rosNode, configFilename, omitOtherTags, defaultTagSize, tfParentFrame));
+    ROS_INFO("ros_markers nodelet is ready. Marker locations will be published on TF when detected.");
+}
+
+void ChilitagsNodelet::rosMarkersDetectionPreemptCallback(){
+    ros_markers_as_ptr_->setPreempted();
+    chilitags_.reset();
+    NODELET_INFO("Stopping to look for chilitags.");
 }
 
 // Register this plugin with pluginlib.  Names must match nodelet_chilitags.xml.
